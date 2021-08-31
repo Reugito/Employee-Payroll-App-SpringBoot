ba9bc8c1a375bac6003284aa7f5393ddaeab6d79
Reugito <raosahebdhotreedu@gmail.com>
2021-08-31 11:50:57 +0530

    EmployeePayrollData and ResponseDTO classes added in EmployeePayrollApp

--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/controller/EmploeePayrollController.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/controller/EmploeePayrollController.java
@@ -2,8 +2,11 @@ package com.bridgelabz.employeepayrollapp.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,19 +29,20 @@ public class EmploeePayrollController
 		return new ResponseEntity<String> ("Get Call Success for id: "+empId, HttpStatus.OK);
 	}
 	
-	@GetMapping("/create")
+	
+	@PostMapping(path = "/create", consumes = {"application/json"})
 	public ResponseEntity<String> addEmployeePayrollData(@RequestBody EmployeePayrollDTO empPayrollDTO)
 	{
 		return new ResponseEntity<String> ("Created Employee payroll Data for: "+empPayrollDTO, HttpStatus.OK);
 	}
 	
-	@GetMapping("/update")
+	@PutMapping("/update")
 	public ResponseEntity<String> updateEmployeePayrollData(@RequestBody EmployeePayrollDTO empPayrollDTO)
 	{
 		return new ResponseEntity<String> ("Updated Employee payroll Data for: "+empPayrollDTO, HttpStatus.OK);
 	}
 	
-	@GetMapping("/delette")
+	@DeleteMapping("/delete")
 	public ResponseEntity<String> deleteEmployeePayrollData(@PathVariable("empId") int empId)
 	{
 		return new ResponseEntity<String> ("Delete Call Success for Id for: "+empId, HttpStatus.OK);
