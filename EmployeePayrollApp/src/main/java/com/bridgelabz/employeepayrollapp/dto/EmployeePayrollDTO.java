56a91228b3a184da42a0a7e41c1f096d85f5f475
Reugito <raosahebdhotreedu@gmail.com>
2021-09-01 16:04:42 +0530

    added validation for every property in EmployeePayrollData class

--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -4,6 +4,9 @@ import java.time.LocalDate;
 import java.util.List;
 
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
 import javax.validation.constraints.Pattern;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -18,13 +21,21 @@ public @ToString class EmployeePayrollDTO
 	@Min(value = 5000, message = "min Wage should be more than 5000")
 	public long salary;
 	
+	@Pattern(regexp = "male|female", message = "gender needs to be male or female")
 	public String gender;
 	
 	@JsonFormat(pattern = "dd MMM yyy")
+	@NotNull(message = "Start date should not be epmty")
+	@PastOrPresent(message = "Start date should pe past or today")
 	public LocalDate startDate;
 	
+	@NotBlank(message = "note cannot be empty")
 	public String note;
+	
+	@NotBlank(message = "profilepic cannot be empty")
 	public String profilePic;
+	
+	@NotNull(message = "department should not be epmty")
 	public List<String> departments;
 
 }
