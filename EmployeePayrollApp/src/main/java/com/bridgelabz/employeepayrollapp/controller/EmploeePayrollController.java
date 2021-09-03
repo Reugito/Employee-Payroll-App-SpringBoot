905fc3055abd4f72fce634144bf3f3e979db75d6
Reugito <raosahebdhotreedu@gmail.com>
2021-09-03 11:27:26 +0530

    added javadoc

--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/controller/EmploeePayrollController.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/controller/EmploeePayrollController.java
@@ -25,7 +25,7 @@ import com.bridgelabz.employeepayrollapp.service.EmployeePayrollService;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
-@RequestMapping("/employeepayrollservice")
+@RequestMapping("/employeepayroll")
 @Slf4j
 public class EmploeePayrollController 
 {
--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/service/EmployeePayrollService.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/service/EmployeePayrollService.java
@@ -16,18 +16,31 @@ public class EmployeePayrollService implements IEmployeePayrollServices{
 	@Autowired
 	private EmployeePayrollRepository employeeReposetory;
 	
+	/**
+	 * @return list of employee information from DB in JSON format
+	 */
 	@Override
 	public List<EmployeePayrollData> getEmployeePayrollData() {
 		return employeeReposetory.findAll();
 	}
 
+	/**
+	 * @param empId - represents employee id
+	 * @return employee information with same empId in JSON format
+	 */
 	@Override
 	public EmployeePayrollData getEmployeePayrollDataById(int empId) {
 		return employeeReposetory
 				.findById(empId)
-				.orElseThrow(() -> new EmployeePayrollException("Employee with employee Id"+empId+"  does not exist!!"));
+				.orElseThrow(() -> new EmployeePayrollException("Employee with employee Id"+empId
+													+"  does not exist!!"));
 	}
 
+	/**
+	 * accepts the employee data in JSON format and stores it in DB
+	 * @param empPayrollDTO - represents object of EmployeePayrollDTO class
+	 * @return accepted employee information in JSON format
+	 */
 	@Override
 	public EmployeePayrollData createEmployeePayrollData(EmployeePayrollDTO empPayrollDTO) {
 		EmployeePayrollData employeePayrollData = null;
@@ -35,6 +48,12 @@ public class EmployeePayrollService implements IEmployeePayrollServices{
 		return employeeReposetory.save(employeePayrollData);
 	}
 
+	/**
+	 * accepts the employee data in JSON format and updates the employee having same empId from database
+	 * @param empId - represents employee id
+	 * @param empPayrollDTO - represents object of EmployeePayrollDTO class
+	 * @return	updated employee information in JSON format
+	 */
 	@Override
 	public EmployeePayrollData updateEmployeePayrollData(int empId, EmployeePayrollDTO empPayrollDTO) {
 		EmployeePayrollData empData = this.getEmployeePayrollDataById(empId);
@@ -43,12 +62,20 @@ public class EmployeePayrollService implements IEmployeePayrollServices{
 		return employeeReposetory.save(empData);
 	}
 
+	/**accepts the empId and deletes the data of that employee from DB
+	 * @param empId - represents employee id
+	 * @return empId and Acknowledgment message
+	 */
 	@Override
 	public void deleteEmployeePayrollData(int empId) {
 		EmployeePayrollData empData = this.getEmployeePayrollDataById(empId);
 		employeeReposetory.delete(empData);
 	}
 
+	/**
+	 * @param department - represents employee department
+	 * @return list of employee information from same department in JSON format
+	 */
 	@Override
 	public List<EmployeePayrollData> getEmploreeByDepartment(String department) {
 		return employeeReposetory.findEmployeeByDepartment(department);
